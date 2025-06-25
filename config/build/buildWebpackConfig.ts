ab873786d91665c6f961406845ee71394e880cfe
Ruscore-Main <oobitovich@mail.ru>
2025-06-25 17:56:45 +0300

    Webpack-dev-server. Переменные окружения (env)

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,14 +1,14 @@
 import { BuildOptions } from "./types/config";
-import path from "path";
 import webpack from "webpack";
 import { buildResolvers } from "./buildResolvers";
 import { buildPlugins } from "./buildPlugins";
 import { buildLoaders } from "./buildLoaders";
+import { buildDevServer } from "./buildDevServer";
 
 export function buildWebpackConfig(
     options: BuildOptions
 ): webpack.Configuration {
-    const { paths, mode } = options;
+    const { paths, mode, isDev } = options;
     return {
         mode: mode,
         entry: paths.entry,
@@ -22,5 +22,7 @@ export function buildWebpackConfig(
             rules: buildLoaders(),
         },
         resolve: buildResolvers(),
+        devtool: isDev ? "inline-source-map" : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined,
     };
 }
