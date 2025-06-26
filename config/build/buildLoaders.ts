93e75e57f867e83399b15c1bd32cdd11c045ca70
Ruscore-Main <oobitovich@mail.ru>
2025-06-26 10:43:02 +0300

    React + sass-loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,9 +1,14 @@
 import webpack from "webpack";
 export function buildLoaders(): webpack.RuleSetRule[] {
+    const sassLoader = {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+    };
+
     const tsLoader = {
         test: /\.tsx?$/,
         use: "ts-loader",
         exclude: /node_modules/,
     };
-    return [tsLoader];
+    return [tsLoader, sassLoader];
 }
